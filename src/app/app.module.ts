22c19407fbe1d432f884dd71a3c29dfd31bc5ca1
TEMFACK DERICK <dericktemfack@gmail.com>
2021-09-13 15:32:16 +0100

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {AmplifyUIAngularModule} from "@aws-amplify/ui-angular";
+import {Amplify} from "@aws-amplify/core";
+import awsconfig from "../aws-exports";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+Amplify.configure(awsconfig);
 
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [AmplifyUIAngularModule, BrowserModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { RouteReuseStrategy } from '@angular/router';
+
+// import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+// import { AppComponent } from './app.component';
+// import { AppRoutingModule } from './app-routing.module';
+
+// @NgModule({
+//   declarations: [AppComponent],
+//   entryComponents: [],
+//   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+//   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
